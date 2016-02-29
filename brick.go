78a66c1799777115f5643ab3c6d9aef9ff7cf87a
Stéphane Travostino <steph@combo.cc>
2016-02-29 02:49:35 +0000

    brick package
    Closes #10

--- brick.go
+++ brick.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/combo/brick/pkg"
 	"gopkg.in/alecthomas/kingpin.v2"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 )
@@ -11,6 +12,9 @@ import (
 var (
 	newCommand = kingpin.Command("new", "Create a new package")
 	newPath    = newCommand.Arg("path", "Directory").Required().String()
+
+	packageCommand = kingpin.Command("package", "Build a package")
+	packagePath    = packageCommand.Arg("path", "Package directory").String()
 )
 
 func abort(message ...interface{}) {
@@ -39,9 +43,41 @@ func doNew(dir string) {
 	}
 }
 
+func doPackage(dir string) {
+	// Create a temporary file for the package
+	packageTmp, err := ioutil.TempFile(dir, "package")
+	if err != nil {
+		abort(err)
+	}
+	defer os.Remove(packageTmp.Name())
+
+	m, err := pkg.Package(packageTmp, dir)
+	if err != nil {
+		abort(err)
+	}
+	packageTmp.Close()
+
+	// Rename package
+	packageFile := filepath.Join(dir, fmt.Sprintf("%s.brick", m.Name))
+	os.Rename(packageTmp.Name(), packageFile)
+
+	fmt.Println("Package written at", packageFile)
+}
+
 func main() {
 	switch kingpin.Parse() {
 	case newCommand.FullCommand():
 		doNew(*newPath)
+
+	case packageCommand.FullCommand():
+		var err error
+		dir := *packagePath
+		if dir == "" {
+			dir, err = os.Getwd()
+			if err != nil {
+				abort(err)
+			}
+		}
+		doPackage(dir)
 	}
 }
